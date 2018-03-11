50f57e3d9eb5effd046e225f6c36663b87b472a6
Marc Hein <info@marc-hein.de>
2018-03-11 21:28:04 +0100

    Added application icon to work on mac

--- PPICalculator.java
+++ PPICalculator.java
@@ -7,16 +7,20 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 public class PPICalculator extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
+            com.apple.eawt.Application.getApplication().setDockIconImage(new ImageIcon("ppi.jpg").getImage());
             Pane root = FXMLLoader.load(getClass().getClassLoader().getResource("ppi.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.setTitle("PPI Calculator");
             primaryStage.getIcons().add(new Image("file:ppi.jpg"));
+            primaryStage.getIcons().add(new Image(PPICalculator.class.getResourceAsStream("ppi.jpg")));
             primaryStage.resizableProperty().set(false);
             primaryStage.show();
         } catch (IOException e) {
@@ -30,3 +34,4 @@ public class PPICalculator extends Application {
     }
 
 }
+
